2527121289e00432dd8241ddc0ff30d5f84f52f7
Emre Ekinci <emree3657@gmail.com>
2025-11-20 01:39:39 +0300

    refactor(skills-section): refactor skills section componet - created skills data in the data folder - created skill cards using map function

--- portfolio/src/pages/home/SkillsSection.tsx
+++ portfolio/src/pages/home/SkillsSection.tsx
@@ -1,3 +1,4 @@
+import { SKILLS } from "../../data/skills";
 import "./SkillsSection.css";
 
 export function SkillsSection() {
@@ -6,45 +7,16 @@ export function SkillsSection() {
       <div className="container">
         <h2 className="section-title">Yetenekler</h2>
         <div className="skills-container">
-          <div className="skill-category">
-            <h3>Front-end</h3>
-            <ul className="skill-list">
-              <li>HTML5 & CSS3</li>
-              <li>JavaScript (ES6+)</li>
-              <li>TypeScript</li>
-              <li>Responsive Tasarım</li>
-              <li>React</li>
-              <li>Redux Tool Kit - RTK Query</li>
-            </ul>
-          </div>
-          <div className="skill-category">
-            <h3>Back-end</h3>
-            <ul className="skill-list">
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>Python (Django/Flask)</li>
-              <li>RESTful API'ler</li>
-            </ul>
-          </div>
-          <div className="skill-category">
-            <h3>Veritabanı</h3>
-            <ul className="skill-list">
-              <li>MongoDB</li>
-              <li>MySQL</li>
-              <li>PostgreSQL</li>
-              <li>Microsoft SQL Server</li>
-              <li>Veritabanı Tasarımı & Yönetimi</li>
-            </ul>
-          </div>
-          <div className="skill-category">
-            <h3>Diğer Araçlar</h3>
-            <ul className="skill-list">
-              <li>Git & GitHub</li>
-              <li>Jest & Testing Library</li>
-              <li>Nginx</li>
-              <li>Docker</li>
-            </ul>
-          </div>
+          {SKILLS.map((skill) => (
+            <div key={skill.category} className="skill-category">
+              <h3>{skill.category}</h3>
+              <ul className="skill-list">
+                {skill.experience.map((experience) => (
+                  <li key={skill.category + "/" + experience}>{experience}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </section>
