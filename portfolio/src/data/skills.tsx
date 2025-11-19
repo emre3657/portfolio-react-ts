5c87a7310aac629555b134fa2dd726fff895c4ae
Emre Ekinci <emree3657@gmail.com>
2025-11-20 01:39:39 +0300

    refactor(skills-section): refactor skills section componet
    - created skills data in the data folder
    - created skill cards using map function

---
+++ portfolio/src/data/skills.tsx
@@ -0,0 +1,35 @@
+export const SKILLS = [
+  {
+    category: "Front-end",
+    experience: [
+      "HTML5 & CSS3",
+      "JavaScript (ES6+)",
+      "Responsive Tasarım",
+      "React",
+      "Redux Tool Kit - RTK Query",
+    ],
+  },
+  {
+    category: "Back-end",
+    experience: [
+      "Node.js",
+      "Express.js",
+      "Python (Django/Flask)",
+      "RESTful API'ler",
+    ],
+  },
+  {
+    category: "Veritabanı",
+    experience: [
+      "MongoDB",
+      "MySQL",
+      "PostgreSQL",
+      "Microsoft SQL Server",
+      "Veritabanı Tasarımı & Yönetimi",
+    ],
+  },
+  {
+    category: "Diğer Araçlar",
+    experience: ["Git & GitHub", "Jest & Testing Library", "Nginx", "Docker"],
+  },
+];
