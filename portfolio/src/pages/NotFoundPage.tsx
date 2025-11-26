f241be10319d2faa30d400f471fbe0b59c531f3b
Emre Ekinci <emree3657@gmail.com>
2025-11-26 23:49:15 +0300

    feat(not-found): update not found page with awesome styles

--- portfolio/src/pages/NotFoundPage.tsx
+++ portfolio/src/pages/NotFoundPage.tsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router";
-import { Header } from "../components/Header";
 import "./NotFoundPage.css";
 
 export function NotFoundPage() {
   return (
-    <>
-      <Header />
-      <div className="not-found-container">
-        <div className="text">Page Not Found</div>
-        <Link className="home-link" to="/">
-          Back to Home
+    <div className="nf-wrapper">
+      <div className="nf-orbit" />
+
+      <div className="nf-card">
+        <div className="nf-404-back">404</div>
+        <div className="nf-404-front">404</div>
+
+        <p className="nf-message">Kaybolmuş gibisin...</p>
+        <p className="nf-sub">
+          Aradığın sayfayı bulamadık. Belki de en iyi yol, ana sayfaya
+          dönmektir.
+        </p>
+
+        <Link to="/" className="nf-button">
+          <i className="fa-solid fa-arrow-left" style={{ marginRight: 8 }} />
+          Anasayfaya Dön
         </Link>
       </div>
-    </>
+    </div>
   );
 }
